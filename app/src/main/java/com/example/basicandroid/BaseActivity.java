02934b547e1c0fb720260418bb3129562d07ab88
ipaddr <i_p_addr@yahoo.com>
2022-04-04 08:45:38 +0700

    remove session

--- app/src/main/java/com/example/basicandroid/BaseActivity.java
+++ app/src/main/java/com/example/basicandroid/BaseActivity.java
@@ -15,13 +15,13 @@ public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        boolean isAllowed = SessionManagerUtil.getInstance()
-                .isSessionActive(this, Calendar.getInstance().getTime());
-        if (!isAllowed) {
-            Intent intent = new Intent(this, Day7Activity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
+//        boolean isAllowed = SessionManagerUtil.getInstance()
+//                .isSessionActive(this, Calendar.getInstance().getTime());
+//        if (!isAllowed) {
+//            Intent intent = new Intent(this, Day7Activity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(intent);
+//        }
         super.onResume();
     }
 }
